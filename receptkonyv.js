9708c6923c4bb940d99739698e09ffbf47622676
T.Ádám <78810601+Mikihiko01@users.noreply.github.com>
2021-09-16 11:18:56 +0200

    Add files via upload

---
+++ receptkonyv.js
@@ -0,0 +1,68 @@
+var receptek=[];
+var hozzavalok=[];
+
+
+$(function(){
+    
+    $.ajax(
+        {url: "etelek.json",
+    success: function(result){
+        console.log(result);
+        receptek=result.receptkonyv;
+        console.log("ajax hivásban");
+        console.log(receptek);
+        megjelenit();
+
+        
+    }
+    }
+    
+    );
+    //console.log("kivül"+receptek);
+    //$("article").append(receptek[0].nev);
+});
+
+function megjelenit(){
+    /*adatok megjelenitése a táblázatban*/
+    $("article").append("<table>");
+    $("section").append("<ul>");
+    $("section div ul").append("<li>");
+    var txt = "<tr id='fejléc'><th>Név</th><th>Elkészitési idő</th><th>Kép</th><th>Leirás</th><th>Hozzávalok</th></tr>";
+    
+    for (var i = 0; i < receptek.length; i++) {
+        txt +="<tr id='"+i+"'>";
+
+       // $("article").append("<table><tr><th>Név</th><th>Elkészitési idő</th><th>kép</th><th>leirás</th></tr></table>");
+        for (var item in receptek[i]) {
+            txt += "<td>"+ receptek[i][item]+"</td>"; 
+        }
+        txt +="</tr>";
+        
+           // $("article table").append("<tr><td>"+receptek[i].nev+"</td><td>"+receptek[i].elkIdo+"</td><td>"+receptek[i].kep+"</td><td>"+receptek[i].leiras+"</td></tr>");
+    }
+    $("article table").append(txt);
+    $("article table tr").hover(hatter);
+    $("tr").on("click",etelkivalaszt);
+    
+    //$("article").append(receptek[0][nev]);
+}
+
+function hatter(){
+    console.log($(this).attr("id"));  
+        $(this).toggleClass("hatterszin");
+  
+
+}
+function etelkivalaszt(){
+    if ($(this).attr("id") !== "fejléc") {
+        sorID = Number($(this).attr("id"));
+        console.log(receptek[sorID].kep);
+        $("#kep img").attr("src",receptek[sorID].kep);
+        $("#kep img").attr("alt",receptek[sorID].nev);
+        
+    }
+    
+
+}
+
+
