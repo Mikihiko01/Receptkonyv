194462bc95c7f4c8e5f18d840b38f9d9fc28ddbc
T.Ádám <78810601+Mikihiko01@users.noreply.github.com>
2021-09-20 14:04:53 +0200

    Add files via upload

--- receptkonyv.js
+++ receptkonyv.js
@@ -1,68 +1,89 @@
-var receptek=[];
-var hozzavalok=[];
+var receptek = [];
+var hozzavalok = [];
 
 
-$(function(){
-    
+$(function () {
+
     $.ajax(
-        {url: "etelek.json",
-    success: function(result){
-        console.log(result);
-        receptek=result.receptkonyv;
-        console.log("ajax hivásban");
-        console.log(receptek);
-        megjelenit();
+        {
+            url: "etelek.json",
+            success: function (result) {
+                console.log(result);
+                receptek = result.receptkonyv;
+                console.log("ajax hivásban");
+                console.log(receptek);
+                megjelenit();
+                $("#bal").click(balraleptet);
+                $("#jobb").click(jobbraleptet);
 
-        
-    }
-    }
-    
+            }
+        }
+
     );
     //console.log("kivül"+receptek);
     //$("article").append(receptek[0].nev);
 });
 
-function megjelenit(){
+function megjelenit() {
     /*adatok megjelenitése a táblázatban*/
     $("article").append("<table>");
     $("section").append("<ul>");
     $("section div ul").append("<li>");
     var txt = "<tr id='fejléc'><th>Név</th><th>Elkészitési idő</th><th>Kép</th><th>Leirás</th><th>Hozzávalok</th></tr>";
-    
-    for (var i = 0; i < receptek.length; i++) {
-        txt +="<tr id='"+i+"'>";
 
-       // $("article").append("<table><tr><th>Név</th><th>Elkészitési idő</th><th>kép</th><th>leirás</th></tr></table>");
+    for (var i = 0; i < receptek.length; i++) {
+        txt += "<tr id='" + i + "'>";
+
+        // $("article").append("<table><tr><th>Név</th><th>Elkészitési idő</th><th>kép</th><th>leirás</th></tr></table>");
         for (var item in receptek[i]) {
-            txt += "<td>"+ receptek[i][item]+"</td>"; 
+            txt += "<td>" + receptek[i][item] + "</td>";
         }
-        txt +="</tr>";
-        
-           // $("article table").append("<tr><td>"+receptek[i].nev+"</td><td>"+receptek[i].elkIdo+"</td><td>"+receptek[i].kep+"</td><td>"+receptek[i].leiras+"</td></tr>");
+        txt += "</tr>";
+
+        // $("article table").append("<tr><td>"+receptek[i].nev+"</td><td>"+receptek[i].elkIdo+"</td><td>"+receptek[i].kep+"</td><td>"+receptek[i].leiras+"</td></tr>");
     }
     $("article table").append(txt);
     $("article table tr").hover(hatter);
-    $("tr").on("click",etelkivalaszt);
-    
+    $("tr").on("click", etelkivalaszt);
+
     //$("article").append(receptek[0][nev]);
 }
 
-function hatter(){
-    console.log($(this).attr("id"));  
-        $(this).toggleClass("hatterszin");
-  
+function hatter() {
+    console.log($(this).attr("id"));
+    $(this).toggleClass("hatterszin");
+
 
 }
-function etelkivalaszt(){
+function etelkivalaszt() {
     if ($(this).attr("id") !== "fejléc") {
         sorID = Number($(this).attr("id"));
         console.log(receptek[sorID].kep);
-        $("#kep img").attr("src",receptek[sorID].kep);
-        $("#kep img").attr("alt",receptek[sorID].nev);
-        
+        $("#kep img").attr("src", receptek[sorID].kep);
+        $("#kep img").attr("alt", receptek[sorID].nev);
+
     }
-    
+
 
 }
+
+function balraleptet() {
+
+    lepteoIndex--;
+    if (lepteoIndex < 0) {
+        lepteoIndex = receptek.length - 1;
+    }
+    megjelinit(lepteoIndex);
+    console.log(lepteoIndex);
+}
+
+function jobbraleptet() {
+    lepteoIndex++;
+    if (lepteoIndex > receptek.length - 1) {
+        lepteoIndex = 0;
+    }
+    megjelinit(lepteoIndex);
+}
+
 
 
